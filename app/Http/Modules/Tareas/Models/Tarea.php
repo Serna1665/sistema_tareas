0d09e33feee1b5141f21310a924177beec430394
serna <cristianserna1606@gmail.com>
2023-11-28 22:06:28 -0500

    add:proyecto de tareas

---
+++ app/Http/Modules/Tareas/Models/Tarea.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Modules\Tareas\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tarea extends Model
+{
+    use HasFactory;
+}
---
+++ app/Http/Modules/Usuarios/Controllers/UsuarioController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Modules\Usuarios\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    //
+}
